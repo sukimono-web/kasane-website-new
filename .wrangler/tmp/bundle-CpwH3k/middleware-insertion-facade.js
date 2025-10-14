6084af8e1a238dd279c8d16927019c6a125d33ca
NatsumiTakezawa <na2mbcor@gmail.com>
2025-10-14 14:41:41 +0900

    fix. 予約リンク修正

---
+++ .wrangler/tmp/bundle-CpwH3k/middleware-insertion-facade.js
@@ -0,0 +1,11 @@
+				import worker, * as OTHER_EXPORTS from "C:\\xampp\\htdocs\\kasane-website-new\\src\\index.ts";
+				import * as __MIDDLEWARE_0__ from "C:\\xampp\\htdocs\\kasane-website-new\\node_modules\\wrangler\\templates\\middleware\\middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "C:\\xampp\\htdocs\\kasane-website-new\\node_modules\\wrangler\\templates\\middleware\\middleware-miniflare3-json-error.ts";
+
+				export * from "C:\\xampp\\htdocs\\kasane-website-new\\src\\index.ts";
+
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default
+				]
+				export default worker;
